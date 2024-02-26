3f9a9e7784b9787872b951c0a08a1922d34ebce7
Usuario <jjjjj@gmail.com>
2024-02-26 20:05:04 +0100

    Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyect0;
+
+/**
+ *
+ * @author Iván Navarro
+ */
+
+public class Calculadora {
+    
+    public static int suma(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int resultado = suma(5, 3);
+        System.out.println("La suma da " + resultado + " en total.");
+    }
+}
+
